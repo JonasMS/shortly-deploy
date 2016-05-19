f49bf1d8f76d8caab6321fa0b92999b8d12f055f
Hack Reactor Students <accounts@hackreactor.com>
2016-05-18 21:32:41 -0700

    set concat settings

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,6 +3,13 @@ module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
     concat: {
+      options: {
+        separator: ';'
+      },
+      dist: {
+        src: ['app/**/*.js'],
+        dest: 'dist/<%= pkg.name %>.js'
+      }
     },
 
     mochaTest: {
